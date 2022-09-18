84157d802896ae2ac362b573b6259f2810cc8015
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2022-09-18 20:03:58 +0300

    input file csv

--- src/components/UploadContacts/UploadContacts.jsx
+++ src/components/UploadContacts/UploadContacts.jsx
@@ -1,7 +1,27 @@
+import { useRef } from "react";
 import { Button } from "reactstrap";
 
 const UploadContacts = (props) => {
-  return <Button color="primary">Загрузить</Button>;
+  const inputFile = useRef(null);
+
+  const onButtonClick = () => {
+    inputFile.current.click();
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        id="file"
+        ref={inputFile}
+        accept=".csv"
+        style={{ display: "none" }}
+      />
+      <Button color="primary" onClick={onButtonClick}>
+        Загрузить
+      </Button>
+    </>
+  );
 };
 
 export default UploadContacts;
