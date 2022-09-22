0c8902b7f13712daf269176be90952cc2fffb516
Demidovez <nikolaydemidovez@example.com>
2022-09-22 13:10:39 +0300

    fix pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,9 +1,8 @@
 import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 import { isIndexShowed } from "utils/utils";
 
-const PaginationCustom = ({allCount = 0, page, moveToPage, countOnPage}) => {
-    const countPages = Math.ceil(allCount / countOnPage);
-
+const PaginationCustom = ({ allCount = 0, page, moveToPage, countOnPage }) => {
+  const countPages = Math.ceil(allCount / countOnPage);
 
   return (
     <div className="d-flex justify-content-between align-items-center">
@@ -34,59 +33,43 @@ const PaginationCustom = ({allCount = 0, page, moveToPage, countOnPage}) => {
                       className={`${index === page ? "active" : ""}`}
                       key={index}
                     >
-                        <PaginationItem>
-                            <PaginationLink
-                                onClick={() => (page - 1 >= 0 ? moveToPage(page - 1) : null)}
-                            >
-                                <i className="fas fa-angle-left"/>
-                            </PaginationLink>
-                        </PaginationItem>
-                        {Array(countPages)
-                            .fill()
-                            .map((_, index) => {
-                                if (countPages <= 5) {
-                                    return (
-                                        <PaginationItem
-                                            className={`${index === page ? "active" : ""}`}
-                                            key={index}
-                                        >
-                                            <PaginationLink onClick={() => moveToPage(index)}>
-                                                {index + 1}
-                                            </PaginationLink>
-                                        </PaginationItem>
-                                    );
-                                } else {
-                                    if (isIndexShowed(page, index, countPages, 3, 5)) {
-                                        return (
-                                            <PaginationItem
-                                                className={`${index === page ? "active" : ""}`}
-                                                key={index}
-                                            >
-                                                <PaginationLink onClick={() => moveToPage(index)}>
-                                                    {index + 1}
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                        );
-                                    } else {
-                                        return null;
-                                    }
-                                }
-                            })}
+                      <PaginationLink onClick={() => moveToPage(index)}>
+                        {index + 1}
+                      </PaginationLink>
+                    </PaginationItem>
+                  );
+                } else {
+                  if (isIndexShowed(page, index, countPages, 3, 5)) {
+                    return (
+                      <PaginationItem
+                        className={`${index === page ? "active" : ""}`}
+                        key={index}
+                      >
+                        <PaginationLink onClick={() => moveToPage(index)}>
+                          {index + 1}
+                        </PaginationLink>
+                      </PaginationItem>
+                    );
+                  } else {
+                    return null;
+                  }
+                }
+              })}
 
-                        <PaginationItem>
-                            <PaginationLink
-                                onClick={() =>
-                                    page + 1 < countPages ? moveToPage(page + 1) : null
-                                }
-                            >
-                                <i className="fas fa-angle-right"/>
-                            </PaginationLink>
-                        </PaginationItem>
-                    </Pagination>
-                </nav>
-            )}
-        </div>
-    );
+            <PaginationItem>
+              <PaginationLink
+                onClick={() =>
+                  page + 1 < countPages ? moveToPage(page + 1) : null
+                }
+              >
+                <i className="fas fa-angle-right" />
+              </PaginationLink>
+            </PaginationItem>
+          </Pagination>
+        </nav>
+      )}
+    </div>
+  );
 };
 
 export default PaginationCustom;
