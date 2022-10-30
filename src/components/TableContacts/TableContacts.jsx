18b291f7f9e62140c7bd88d1aee2030f828b2122
Nikolay Demidovez <nikolaydemidovez@gmail.com>
2022-10-30 20:56:35 +0300

    array of sequences

--- src/components/TableContacts/TableContacts.jsx
+++ src/components/TableContacts/TableContacts.jsx
@@ -219,7 +219,7 @@ const TableContacts = ({
                           ...field.style,
                         }}
                       >
-                        {contact[field.name]}
+                        {(contact[field.name] || []).join(", ")}
                       </td>
                     );
                   } else {
